799e15af94b6ea724c8579d456b4a133c97994d0
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2019-11-23 05:39:39 -0800

    Project: Fixed file name with bias and sequence length

--- project/src/pubn/_utils.py
+++ project/src/pubn/_utils.py
@@ -109,17 +109,18 @@ def construct_filename(prefix: str, args: Namespace, out_dir: Path, file_ext: st
 
     fields = [prefix] if prefix else []
     fields += [f"n-p={args.size_p}", f"n-n={args.size_n}", f"n-u={args.size_u}",
-               f"pos={_classes_to_str(args.pos)}", f"neg={_classes_to_str(args.neg)}",
-               f"seq={args.seq_len}"]
+               f"pos={_classes_to_str(args.pos)}", f"neg={_classes_to_str(args.neg)}"]
 
     if args.bias:
-        # Ensure bias has same order as
-        bias_sorted = [x for _, x in sorted(args.bias)]
+        bias_sorted = [x for _, x in args.bias]
         bias_str = ','.join([f"{x:.2f}" for x in bias_sorted])
         fields.append(f"bias={bias_str}")
 
     if args.preprocess:
-        fields.append("pp")
+        fields.append("preprocess")
+    else:
+        # Sequence length only matters if LSTM
+        fields.append(f"seq={args.seq_len}")
 
     if add_timestamp:
         time_str = time.strftime("%Y-%m-%d-%H-%M-%S")
