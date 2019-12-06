cbdd82c5255e13ffa2743559eab66bb76c3a612d
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2019-12-06 13:58:35 -0800

    Project: Fixed issues in build submission script

--- project/build_submission.sh
+++ project/build_submission.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env zsh
 
 SUBMISSION_NAME=project
-SUBMISSION_DIR=$(pwd)/${SUBMISSION_DIR}
+SUBMISSION_DIR=$(pwd)/zayd_hammoudeh_${SUBMISSION_NAME}
 
 TEX_DIR=$(pwd)/tex
+TEX_BASE=${SUBMISSION_NAME}
 TEX_FILE=${SUBMISSION_NAME}.tex
 
 SRC_DIR=$(pwd)/src
 
 function build_submission_dir() {
     printf "Deleting and recreating submission directory..."
-    rm -rf ${SUBMISSION_DIR}
-    mkdir -p ${SUBMISSION_DIR}
+    rm -rf ${SUBMISSION_DIR} > /dev/null
+    mkdir -p ${SUBMISSION_DIR} > /dev/null
     printf "COMPLETED\n"
 }
 
@@ -21,11 +22,13 @@ function build_src() {
     cp -r ${SRC_DIR} ${SUBMISSION_DIR} > /dev/null
     printf "COMPLETED\n"
 
-    cd ${SUBMISSION_DIR}/${SRC_DIR} > /dev/null
+    printf "Deleting files that should not be in submission source code..."
+    cd ${SUBMISSION_DIR}/$(basename ${SRC_DIR}) > /dev/null
     ITEMS_TO_DELETE=(".idea" ".data" "tensors" "tb" "__pycache__" "logs" "results" "models")
     for DEL_OBJ in "${ITEMS_TO_DELETE[@]}"; do
         rm -rf ${DEL_OBJ} > /dev/null
     done
+    printf "COMPLETED\n"
 
     cd - > /dev/null
 }
@@ -39,18 +42,19 @@ function build_tex() {
     # Remove the PDF for good measure
     rm -rf ${TEX_BASE}.pdf > /dev/null
     printf "COMPLETED\n"
+
     printf "Compiling the LaTeX submission..."
-    latexmk ${TEX_FILE} > /dev/null
+    latexmk ${TEX_FILE} &> /dev/null
 
     if [ $? -ne 0 ]; then
         printf "\n"
         msg="ERROR: Building ${TEX_FILE}"
         printf "\e[01;31m${msg}\n\e[0m" >&2
     else
-        cp ${TEX_BASE}.pdf ${SUBMISSION_DIR} > /dev/null
+        cp ${TEX_BASE}.pdf ${SUBMISSION_DIR}/zayd_hammoudeh_${SUBMISSION_NAME}.pdf > /dev/null
         printf "COMPLETED\n"
     fi
-    cd -
+    cd - > /dev/null
 }
 
 function zip_submission() {
